c8056b92556da32b59f0b2486b365b58f3abccba
Ifeanyi Ofoefule <ofoefulec_fny@yahoo.com>
2021-07-29 17:26:55 +0100

    started side menu navigation, modified drawer navigation

---
+++ app/navigations/SideMenu/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Container from "../../components/common/Container";
+import { SETTINGS } from "../../constants/routeNames";
+import styles from "./styles";
+
+const SideMenu = ({ navigation }) => {
+  const menuItems = [
+    {
+      icon: <Text>T</Text>,
+      name: "Settings",
+      onPress: () => {
+        navigation.navigate(SETTINGS);
+      },
+    },
+    { icon: <Text>T</Text>, name: "Logout" },
+  ];
+
+  return (
+    <SafeAreaView>
+      <Container>
+        <Image
+          height={70}
+          width={70}
+          source={require("../../../assets/images/logo.png")}
+          style={styles.logoImage}
+        />
+
+        <View style={{ paddingHorizontal: 70 }}>
+          {menuItems.map(({ name, icon, onPress }) => (
+            <TouchableOpacity onPress={onPress} key={name} style={styles.item}>
+              {icon}
+              <Text style={styles.itemText}> {name} </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </Container>
+    </SafeAreaView>
+  );
+};
+
+export default SideMenu;
--- app/screens/Contacts/index.js
+++ app/screens/Contacts/index.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useNavigation, Icon } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import Container from "../../components/common/Container";
 
+import colors from "../../../assets/theme/colors";
+
 const Contacts = () => {
+  const { setOptions, toggleDrawer } = useNavigation();
+
+  useEffect(() => {
+    setOptions({
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => toggleDrawer()}>
+          <Text style={{ padding: 10, color: "#000" }}>NAV</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
   return (
     <Container>
       <Text>Hi from Contacts</Text>
